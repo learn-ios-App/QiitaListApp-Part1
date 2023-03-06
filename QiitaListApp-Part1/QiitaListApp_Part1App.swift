b68b10a89be08257ef55ab9f324ad3ca87756bf6
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-06 20:15:10 +0900

    Initial Commit

---
+++ QiitaListApp-Part1/QiitaListApp_Part1App.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaListApp_Part1App.swift
+//  QiitaListApp-Part1
+//
+//  Created by 渡邊魁優 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct QiitaListApp_Part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
